37c57b943eeb935b525e44a777b2b9a153716cf5
matiasbastidas <matias.bastidasm@gmail.com>
2022-08-07 22:28:55 -0400

    random2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var random = 0;
-function onClick () {
-    var random = Math.floor(Math.random() * 100) + 1;
-    document.getElementById("numerorandom").innerHTML = random;
-    console.log(random);
+const onClick = (random = 0) => {
+    random = Math.floor(Math.random() * 100) + 1
+    document.getElementById("numerorandom").innerHTML = random
+    return random
 }
+
